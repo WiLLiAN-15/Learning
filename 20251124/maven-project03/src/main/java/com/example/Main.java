2aa8687c4ad6b4f1926d5edc48070661c33ae5ca
Chuck <beier33@163.com>
2025-11-23 10:49:41 +0800

    Learning 20251123 Source

---
+++ 20251124/maven-project03/src/main/java/com/example/Main.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/***
+ *@title $NAME
+ *@description <TODO description class purpose>
+ *@author Chuck Xu
+ *@version 1.0.0
+ *@create 2025/11/23 10:37
+ *@mail beier33@163.com
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
