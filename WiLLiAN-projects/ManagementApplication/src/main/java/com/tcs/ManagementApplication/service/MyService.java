3925451a5a83953dc7be9d7629e7d4cac2ba3403
WiLLiAN-15 <williankids2000@gmail.com>
2025-12-16 18:25:21 +0900

    service

--- WiLLiAN-projects/ManagementApplication/src/main/java/com/tcs/ManagementApplication/service/MyService.java
+++ WiLLiAN-projects/ManagementApplication/src/main/java/com/tcs/ManagementApplication/service/MyService.java
@@ -1,4 +1,23 @@
 package com.tcs.ManagementApplication.service;
 
-public interface MyService {
+import com.tcs.ManagementApplication.mapper.MyMapper;
+import com.tcs.ManagementApplication.pojo.Department;
+import com.tcs.ManagementApplication.pojo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MyService {
+  @Autowired
+  private MyMapper myMapper;
+  
+  public List<Employee> findAllEmployee() {
+    return myMapper.findAllEmployee();
+  }
+
+  public List<Department> findAllDepartment() {
+    return myMapper.findAllDepartment();
+  }
 }
